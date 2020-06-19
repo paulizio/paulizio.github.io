7ea3554f54ea8e686715e9b840a39344cad12975
paulizio <pauli.nordman@hotmail.com>
2020-06-19 10:06:29 +0300

    Updates

---
+++ precache-manifest.5513b05b317ff0be517e36a722e0fba7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "afcebe4769783d9bc589a506cea0ac4d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "de3583a21e48b3b03105",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "7bdcba80b1fbcb4923f2",
+    "url": "/static/js/2.42a3760b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.42a3760b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "de3583a21e48b3b03105",
+    "url": "/static/js/main.3b733a5d.chunk.js"
+  },
+  {
+    "revision": "9ec32b301416206a23c6",
+    "url": "/static/js/runtime-main.e4993ad5.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
